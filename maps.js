1dce3949f031686784c87611b8ea3de97f8abbbe
berna39 <kalemajoseph39@gmail.com>
2022-12-17 11:18:31 +0200

    Maps

---
+++ maps.js
@@ -0,0 +1,13 @@
+let countries = new Map([
+    ['Germany', 340],
+    ['UK', 312],
+    ['France', 287],
+    ['Spain', 240]
+]);
+
+countries.set('Belgium', 197);
+
+console.log(countries.get('UK')); // 312
+console.log(countries.has('Spain')); // true
+
+countries.forEach((value, key) => console.log(`${key} -> ${value}`)); // print values only
