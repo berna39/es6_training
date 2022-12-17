400abbd27533186ffe0f7d7526cc31eb9da2b8d7
berna39 <kalemajoseph39@gmail.com>
2022-12-17 11:12:23 +0200

    For/Of Loop

---
+++ forof.js
@@ -0,0 +1,10 @@
+let uk = ['London', 'Manchester', 'Bristo', 'Hull', 'Licester'];
+let name = 'The Patient Mwenge';
+
+for(town of uk){
+    console.log(town);
+}
+
+for(letter of name){
+    console.log(letter);
+}
