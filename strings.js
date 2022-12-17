a400119ae0c6a1d34b1a776591379cde8b09824d
berna39 <kalemajoseph39@gmail.com>
2022-12-17 11:57:56 +0200

    String methods

---
+++ strings.js
@@ -0,0 +1,5 @@
+let text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry";
+
+console.log(text.includes('text')); // text
+console.log(text.startsWith('lorem')); // false -> case sensitive
+console.log(text.endsWith('industry')); // true
