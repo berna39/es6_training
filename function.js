976741ca1d5ee82d518f0201ba80dbd1302de698
berna39 <kalemajoseph39@gmail.com>
2022-12-17 11:54:24 +0200

    Function's arguments

---
+++ function.js
@@ -0,0 +1,18 @@
+// Default parameter
+function loop(value = 1){
+    for (let index = 0; index < value; index++) {
+        console.log(index);
+    }
+}
+
+loop(); // It will consider the default value (1)
+loop(4);
+
+// To treat an infinite number of parameters
+function sum(...args){
+    let sum = 0;
+    for(let value of args) sum += value;
+    return sum; 
+}
+
+console.log(sum(1,2,3,4,5));
---
+++ symbols.js
@@ -0,0 +1,1 @@
+// To generate a unique value for an object
