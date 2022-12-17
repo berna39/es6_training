7262f40b0c95bc56c4f2e9fed5172b12c2860372
berna39 <kalemajoseph39@gmail.com>
2022-12-17 11:01:04 +0200

    Variable scope

---
+++ variableScope.js
@@ -0,0 +1,18 @@
+// variable scope example
+
+function loop(number){
+    for(var i=0; i <= number; i++){
+        console.log(i);
+    }
+    console.log(i); // this will print 6 because the scope of var i is the function
+}
+
+function otherLoop(number){
+    for(let i=0; i <= number; i++){
+        console.log(i);
+    }
+    //console.log(i); // this will throw an error
+}
+
+//loop(5);
+otherLoop(5);
