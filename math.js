4da72630012e6316cea0da7347869aaa52012cc9
berna39 <kalemajoseph39@gmail.com>
2022-12-17 12:23:49 +0200

    Math & Numbers

---
+++ math.js
@@ -0,0 +1,5 @@
+let pi = 3.14;
+
+console.log(Math.trunc(pi)); // returns the integer part of pi
+console.log(Math.cbrt(27)); // returns the cube root
+console.log(Math.log2(2)); // returns the base 2 logarithm
---
+++ numbers.js
@@ -0,0 +1,9 @@
+let min = Number.MIN_SAFE_INTEGER;
+let max = Number.MAX_SAFE_INTEGER;
+
+console.log(min); 
+console.log(max);
+
+console.log(Number.isInteger(12.3)); // false
+console.log(Number.isInteger(1242342342342332342342342324)); // true -> it's a integer somehow :) 
+console.log(Number.isSafeInteger(124234234234233245252234)); // false -> it's out of the range
